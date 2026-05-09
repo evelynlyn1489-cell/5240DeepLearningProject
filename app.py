22f5fa8813ed1d56f56e6b3bb3564cf4db5da053
evelynlyn1489-cell <evelynlyn1489@gmail.com>
2026-05-09 17:37:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def text2audio(story_text):
 # ============================================================
 # Main part
 # ============================================================
-st.set_page_config(page_title="Your Image to Audio Story", page_icon="🧸")
+st.set_page_config(page_title="Your Image to Audio Story", page_icon="https://icons8.com/icon/7xE1Vu0MCgzW/story-book")
 st.header("🌈 Magic Story Time! 🧸")
 st.markdown("Hi there, little friend! 🎉 Pick a picture and I'll tell you a fun story!")
 
